6180ad4e6d9dd998f919345a5d448976481ef54a
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2023-11-09 13:48:00 +0530

    DOM Practice delete

---
+++ JS6_DOM/players.js
@@ -0,0 +1,50 @@
+var players = [
+    {
+        name : "Rohit",
+        noOfRuns : 40,
+        noOfBalls : 24,
+        noOfFours : 6,
+        noOfSixes : 2,
+        strikeRate : 166.67
+    },
+    {
+        name : "Ghill",
+        noOfRuns : 23,
+        noOfBalls : 24,
+        noOfFours : 4,
+        noOfSixes : 1,
+        strikeRate : 95.83
+    },
+    {
+        name : "kohli",
+        noOfRuns : 101,
+        noOfBalls : 121,
+        noOfFours : 10,
+        noOfSixes : 0,
+        strikeRate : 83.47
+    },
+    {
+        name : "Iyer",
+        noOfRuns : 77,
+        noOfBalls : 87,
+        noOfFours : 7,
+        noOfSixes : 2,
+        strikeRate : 88.51
+    },
+    {
+        name : "Rahul",
+        noOfRuns : 8,
+        noOfBalls : 17,
+        noOfFours : 0,
+        noOfSixes : 0,
+        strikeRate : 47.06
+    },
+    {
+        name : "Sky",
+        noOfRuns : 22,
+        noOfBalls : 14,
+        noOfFours : 5,
+        noOfSixes : 0,
+        strikeRate : 157.14
+    }
+]
